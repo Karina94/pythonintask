88c85186fc880322a5000dbdd4eb88e87060aa4d
Karina94 <karisha22_94@mail.ru>
2017-04-16 01:36:51 +0300

    Шатунова К.Э. 26вар 1-3 задания

---
+++ ist/task_2_26.py
@@ -0,0 +1,6 @@
+# Shatunova Karina Eduardovna
+# Variant 26
+# task 2
+print ("Берегите Братство, это святое общение в Церкви")
+print ("\t\t\t\t\t\t\t\t\t\t\t\t\tИннокентий (Тихонов)")
+input("\n\nНажмите Enter, чтобы выйти.")
